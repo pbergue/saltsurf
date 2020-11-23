77d2a3f383ffc4eafac381824df4dcafdab426c7
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-23 18:39:10 +0100

    routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :spots, :only [:index, :show] do
+    resources :sessions, :only []
+  end
+
+  resources :spots, :only [] do
+    resources :favorites, :only []
+  end
+
+  resources :profile, :only [:show ]
+
 end
