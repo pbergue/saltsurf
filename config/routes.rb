cad08725996c179d004454c69ae08424256e12ab
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-24 11:13:40 +0100

    routes updated, namely resource :profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :spots, :only [:index, :show] do
-    resources :sessions, :only []
+  resources :spots, only: [:index, :show] do
+    resources :sessions, only: [:create]
+    resources :favorites, only: [:create]
   end
 
-  resources :spots, :only [] do
-    resources :favorites, :only []
-  end
+  resource :profile, only: [:show ]
 
-  resources :profile, :only [:show ]
+  # if we want to update sessions uncomment the following :
+  # resources :sessions, only: [:edit, :update]
 
 end
