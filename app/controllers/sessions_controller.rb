ad8f60763cc3e08f7e0668475aa1dfb65a4163d1
Pierre Bergue <pierre.bergue@gmail.com>
2020-11-24 12:08:33 +0100

    created the Create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def create
+    @spot = Spot.find(params[:id])
+    @session = Session.new(session_params)
+    @user = current_user
+    @spot.session = @session
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:title, :description, :rating, :timestamp)
   end
 end
