c5598b575cc6e657621ed7ced40c0b4b8ec862d3
cameron brock <cameronb@usc.edu>
2020-11-24 11:19:35 +0100

    validations added

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,3 +1,8 @@
 class Forecast < ApplicationRecord
   belongs_to :spot
+
+  validates :spot_id, :source, :timestamp, presence: true
+  validates :wave_height, :wind_direction, :wind_speed, :swell_height, :swell_direction, :period, numericality:true
+  
+  #validates :low_tide, :high_tide #not sure how we should validate
 end
