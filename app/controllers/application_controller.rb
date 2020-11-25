39f1c637c4e1d3914ecb9ce45dae8cbfa8a40d51
Pierre Bergue <pierre.bergue@gmail.com>
2020-11-25 17:22:07 +0100

    created the profile view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,7 @@ class ApplicationController < ActionController::Base
     # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 
+  def after_sign_in_path_for(resource)
+    profile_path
+  end
 end
