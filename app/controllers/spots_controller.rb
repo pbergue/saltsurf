96b943c6960d9a465012424f354279e16acd6e7c
oliver coton <oliver.coton@gmail.com>
2020-11-25 12:28:16 +0100

    updated spots controller to include mapbox and geocoder

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -4,6 +4,12 @@ class SpotsController < ApplicationController
 
   def index
     @spots = Spot.all
+
+    @markers = @spots.geocoded.map do |spot|
+    {
+      lat: spot.latitude,
+      lng: spot.longitude
+    }
   end
 
   def show
