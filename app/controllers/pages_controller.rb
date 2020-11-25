1b9f398a80a7875ef3d8fb9801fcea3a30635405
cameron brock <cameronb@usc.edu>
2020-11-25 09:39:08 +0100

    front page done except for photos working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @spots = Spot.all
+    @spots = @spots.first(4) #must be replaced with Top 5 function
   end
 end
