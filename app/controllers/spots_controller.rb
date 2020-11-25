35168c0c368ae363f9f42adefd6c4c5d4c0089b4
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-25 16:55:21 +0100

    distance calculation in index

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -4,16 +4,18 @@ class SpotsController < ApplicationController
 
   def index
     @spots = Spot.all
+
+    if params[:query].present?
+      @spots = @spots.near(params[:query], params[:distance].to_i)
+      @query_coordinates = Geocoder.search(params[:query]).first&.coordinates
+      console
+    end
+
     @markers = @spots.geocoded.map do |spot|
-    {
-      lat: spot.latitude,
-      lng: spot.longitude
-    }
-      if params[:query].present?
-        @spots = spots.near(params[:query],5)
-      else
-        @spots = Spot.all
-      end
+      {
+        lat: spot.latitude,
+        lng: spot.longitude
+      }
     end
   end
 
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -6,6 +6,6 @@ class Spot < ApplicationRecord
 
   validates :latitude, :longitude, :name, :description, presence: true
   geocoded_by :name
-  after_validation :geocode, if: :will_save_change_to_name?
+  # after_validation :geocode, if: :will_save_change_to_name?
 
 end
