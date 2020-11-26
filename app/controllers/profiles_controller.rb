6d15ec6f081671471c73bcc0300d2549a58ec8cc
Pierre Bergue <pierre.bergue@gmail.com>
2020-11-26 12:59:30 +0100

    modified user model to access spots through sessions and added a working filter for sessions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,16 @@
 class ProfilesController < ApplicationController
 
   before_action(:set_profile, only: [:show])
+  # has_scope :by_favorites
 
   def show
     @favorites_spots = current_user.spots
-    @sessions = current_user.sessions
+    @session_spots = current_user.session_spots
+    if params[:session_spot_name]
+      @sessions = Session.where(spot_id: params[:session_spot_name])
+    else
+      @sessions = current_user.sessions
+    end
   end
 
   private
@@ -12,6 +18,14 @@ class ProfilesController < ApplicationController
   def set_profile
     @profile = current_user
   end
+
+  # def favorites_list
+  #   favorites = []
+  #   Favorites.all.each do |favorite|
+  #     favorites << favorite.spot.name
+  #   end
+  #   favorites.uniq
+  # end
 
   # The later can be replaced directly in the view with present? method
   # def owner_has_no_sessions?
