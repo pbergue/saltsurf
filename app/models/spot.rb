a29b80cf21555a9068985bca1b3df98c04cb9458
Jean Laine <lainecaroff.jean@gmail.com>
2020-11-27 13:21:43 +0100

    order by rating functionnal

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -7,4 +7,12 @@ class Spot < ApplicationRecord
   validates :latitude, :longitude, :name, :description, presence: true
   geocoded_by :name
   # after_validation :geocode, if: :will_save_change_to_name?
+
+
+  def forecast_today
+    self.forecasts.find do |forecast|
+      forecast.timestamp.day == Date.today.day
+    end
+
+  end
 end
