c65a7e30094ed7847d8c7076f7f4c69f1aec5093
oliver coton <oliver.coton@gmail.com>
2020-11-30 18:25:51 +0100

    removed a js listener

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 require("custom/_slider")
 require('places.js')
 require("custom/_radio_show")
-require("custom/show_update_form")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
