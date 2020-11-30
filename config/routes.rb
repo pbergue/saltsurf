25850b2c8514b934fe0d23e338a8cb6a06233b50
cameron brock <cameronb@usc.edu>
2020-11-30 12:09:34 +0100

    moved update for sessoins in controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :spots, only: [:index, :show] do
-    resources :sessions, only: [:create, :update]
+    resources :sessions, only: [:create]
     resources :favorites, only: [:create, :destroy]
   end
 
